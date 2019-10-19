73b8fc163a30fd8b51a5bb8895b3aea9534cc906
Deon Brewis <deon@mylio.com>
2019-10-19 14:52:07 -0700

    Add health route

--- run.py
+++ run.py
@@ -8,6 +8,10 @@ spider = JumpingSpider.JumpingSpider()
 def get_companies():
   return spider.go()
 
+@api.route('/health', methods=['GET'])
+def get_health():
+  return "healthy"
+
 if __name__ == '__main__':
     api.run(host='0.0.0.0', port=8080, debug=True)
 
