f7b48d7298c7e5153a78be4a11a6413e7dc0423f
Deon Brewis <deon@mylio.com>
2019-10-19 18:30:34 -0700

    Typo

--- JumpingSpider.py
+++ JumpingSpider.py
@@ -28,7 +28,7 @@ class JumpingSpider:
         result = await self.control.spinMotor(0, 5, 0)
         result = await self.control.spinMotor(1, 5, 0)
         result = await self.control.spinMotor(2, 5, 0)
-        result = await self.control.spinMotor(3, 5, 01)
+        result = await self.control.spinMotor(3, 5, 0)
         print("Go " + str(self.iter) + " done")
         return result
 
