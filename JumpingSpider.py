e49c3ef9aa25a6c7f2d4becaa138ef025cee99ec
Deon Brewis <deon@mylio.com>
2019-10-27 16:49:19 -0700

    2 secons

--- JumpingSpider.py
+++ JumpingSpider.py
@@ -69,7 +69,7 @@ class JumpingSpider:
         result = await self.control.spinMotor(0, 30, 1, lambda: self.control.isButtonPressed(0))
         self.control.debugOn(3)
 
-        result = await self.control.spinMotor(0, 1, 1)
+        result = await self.control.spinMotor(0, 2, 1)
 
         if self.audioPlayer:
             self.audioPlayer.wait()
